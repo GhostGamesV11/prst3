533f80122f8ee2d9967216a3edfcd1f48eaadd34
GhostGamesV11 <47499005+GhostGamesV11@users.noreply.github.com>
2021-03-18 17:20:43 +0100

    Add files via upload

---
+++ roz2.2.6/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int potega(int,int);
+
+int main()
+{
+    int a,b;
+    printf("Podaj dwie liczby nieujemne rozne od zera:\n");
+    printf("Podaj pierwsza liczbe:");
+    scanf("%d",&a);
+    printf("Podaj druga liczbe:");
+    scanf("%d",&b);
+    printf("Wynik to %d",potega(a,b));
+    return 0;
+}
+
+int potega(int n,int m)
+{
+    int wynik=1;
+    for(int i=m; i!=0;i--)
+        wynik*=n;
+    return wynik;
+}
